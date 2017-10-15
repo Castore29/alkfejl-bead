33d48af8ccc3f16724d5290d8e429bed96b2a1f3
Kern Krisztián <krisz.kern@gmail.com>
2017-10-15 22:39:16 +0200

    project init

---
+++ fishing-shop/src/main/java/edu/ik/alkfejl/fishingshop/FishingShopApplication.java
@@ -0,0 +1,12 @@
+package edu.ik.alkfejl.fishingshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingShopApplication.class, args);
+	}
+}
