3207ee831bea806e66256bd4f6fa0e48dd43c50d
Kern Krisztián <krisz.kern@gmail.com>
2017-10-20 18:42:15 +0200

    db-modellek letrehozasa

---
+++ fishing-shop/src/main/java/edu/ik/alkfejl/fishingshop/model/BaseEntity.java
@@ -0,0 +1,39 @@
+package edu.ik.alkfejl.fishingshop.model;
+
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.Version;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@MappedSuperclass
+@EqualsAndHashCode(of = {"id"})
+public abstract class BaseEntity {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+    @Version
+	private long version;
+    
+    private Date createDate;
+    
+    private boolean active;
+    
+    @PrePersist
+    void preInsert() {
+    	if (this.createDate == null) {
+    		this.createDate = new Date();
+    		this.active = true;
+    	}
+    }
+
+}
---
+++ fishing-shop/src/main/java/edu/ik/alkfejl/fishingshop/model/Tag.java
@@ -0,0 +1,34 @@
+package edu.ik.alkfejl.fishingshop.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "TAGS")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"tagName"})
+public class Tag {
+	
+    @Id
+    @Column(nullable = false, unique = true)
+	private String tagName;
+    
+    @ManyToMany(mappedBy="tags")
+    @JsonIgnore
+    private Set<Product> products;
+
+}
